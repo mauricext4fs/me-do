ab38b28bdfc127516861735565c77239a1e68249
Maurice Courtois <courtoisninja@gmail.com>
2025-06-16 15:43:37 +0200

    Adding all the stuff that could be used from Pomodoro code

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"me-do/repository"
 	"os"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -17,7 +19,14 @@ type TODO struct {
 	DB         repository.Repository
 	MainWindow fyne.Window
 	UIElements UIElements
+	Countdown  Countdown
 	ID         int64
+	Stop       bool
+}
+
+type Countdown struct {
+	Minute int64
+	Second int64
 }
 
 func main() {
@@ -39,6 +48,67 @@ func main() {
 	td.MainWindow.SetMaster()
 
 	td.MainWindow.ShowAndRun()
+}
+
+func (td *TODO) Animate(co fyne.CanvasObject, win fyne.Window) {
+	tick := time.NewTicker(time.Second)
+	go func() {
+		for !td.Stop {
+			td.Layout(nil, co.Size())
+			<-tick.C
+			td.CountdownDown()
+			td.UIElements.CountDownText.UpdateText(fmt.Sprintf("%d : %d", td.Countdown.Minute, td.Countdown.Second))
+		}
+		if td.Countdown.Minute == 0 && td.Countdown.Second == 0 {
+			err := td.DB.UpdateActivity(td.ID, repository.Activities{ID: td.ID, EndTimestamp: time.Now()})
+			if err != nil {
+				log.Fatal("Error updating activity to sqlite DB: ", err)
+			}
+
+			if td.App.Preferences().FloatWithFallback("withSound", 1) == 1 {
+				//PlayNotificationSound()
+			}
+
+			if td.App.Preferences().FloatWithFallback("withNotification", 0) == 1 {
+				n := fyne.NewNotification("Task finished!", "Another task completed. Congrats!")
+				app.New().SendNotification(n)
+			}
+
+		}
+	}()
+}
+
+func (td *TODO) Reset(win fyne.Window, newTitle string) {
+	// Stop any existing counter (if any)
+	td.Stop = true
+	time.Sleep(1 * time.Second)
+	td.Countdown.Minute = 24
+	td.Countdown.Second = 59
+	td.UIElements.CountDownText.UpdateText("25 Minutes")
+
+	td.UpdateStartStopButton("Start Task", false)
+	if win != nil && newTitle != "" {
+		fyne.Window.SetTitle(win, newTitle)
+	}
+}
+
+func (td *TODO) UpdateTODOCount() {
+	//count, err := td.DB.CountCompletedTODO()
+	count := 0
+	/*if err != nil {
+		log.Fatal("Error getting count of completed tasks from sqlite DB: ", err)
+	}*/
+	td.UIElements.TODOCountLabel.SetText(fmt.Sprintf("Completed Tasks: %d", count))
+}
+
+func (td *TODO) CountdownDown() {
+	td.Countdown.Second--
+	if td.Countdown.Minute >= 1 && td.Countdown.Second <= 0 {
+		td.Countdown.Minute--
+		td.Countdown.Second = 59
+	} else if td.Countdown.Minute == 0 && td.Countdown.Second <= 0 {
+		td.Stop = true
+	}
 }
 
 func (p *TODO) connectSQL() (*sql.DB, error) {
