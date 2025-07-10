46111364960dc17ed29f3eee6c77b360552de823
Maurice Courtois <courtoisninja@gmail.com>
2025-07-10 17:40:33 +0200

    WP: Adding TODO tab

---
+++ todoTab.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"me-do/repository"
+	"strconv"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func (td *TODO) todoTab() *fyne.Container {
+	//td.LoadTasks()
+	td.Tasks = td.getTasksSlice()
+	td.TaskTable = td.getTasksTable()
+
+	tasksTableContainer := container.NewBorder(
+		nil,
+		nil,
+		nil,
+		nil,
+		container.NewAdaptiveGrid(1, td.TaskTable),
+	)
+
+	return tasksTableContainer
+}
+
+func (td *TODO) getTasksTable() *widget.Table {
+	t := widget.NewTable(
+		func() (int, int) {
+			return len(td.Tasks), len(td.Tasks[0])
+		},
+		func() fyne.CanvasObject {
+			ctr := container.NewVBox(widget.NewLabel(""))
+			return ctr
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			o.(*fyne.Container).Objects = []fyne.CanvasObject{
+				widget.NewLabel(td.Tasks[i.Row][i.Col].(string)),
+			}
+		})
+
+	colWidths := []float32{70, 700, 70}
+	for i := 0; i < len(colWidths); i++ {
+		t.SetColumnWidth(i, colWidths[i])
+	}
+
+	return t
+}
+
+func (td *TODO) getTasksSlice() [][]interface{} {
+	var slice [][]interface{}
+
+	tasks, err := td.currentTasks()
+	if err != nil {
+		log.Println("err: ", err)
+	}
+
+	slice = append(slice, []interface{}{"Position", "Title", "Priority"})
+
+	for _, x := range tasks {
+		var currentRow []interface{}
+
+		currentRow = append(currentRow, strconv.FormatInt(x.ID, 10))
+		currentRow = append(currentRow, fmt.Sprintf("%s", x.Title))
+		currentRow = append(currentRow, fmt.Sprintf("%s", x.Priority))
+
+		slice = append(slice, currentRow)
+	}
+
+	return slice
+}
+
+func (td *TODO) currentTasks() ([]repository.Tasks, error) {
+	tasks, err := td.DB.AllTasks()
+	if err != nil {
+		log.Println("Error: ", err)
+		return nil, err
+	}
+
+	return tasks, nil
+}
