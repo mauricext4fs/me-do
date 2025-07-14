8280f527467416a5ed718bce46c30ae523e0d06f
Maurice Courtois <courtoisninja@gmail.com>
2025-07-14 16:36:24 +0200

    Add task.go

---
+++ task.go
@@ -0,0 +1,5 @@
+package main
+
+var taskPriority = []string{"", "Critical", "Very High", "High", "Medium", "Low"}
+var taskStatus = []string{"Not started", "In Progress", "Paused", "Stuck", "Done"}
+var TODOColums = []string{"ID", "Position", "Title", "Status", "Priority"}
