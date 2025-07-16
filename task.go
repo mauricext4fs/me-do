22e939676611164dd0bee34608fe8b4b1a9c20db
Maurice Courtois <courtoisninja@gmail.com>
2025-07-16 23:01:02 +0200

    WP: Switch Status field to our new custom Select

--- task.go
+++ task.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-	"log"
+	"image/color"
 
-	"fyne.io/fyne/v2/widget"
+	"golang.org/x/image/colornames"
 )
 
 var taskPriority = []string{"", "Critical", "Very High", "High", "Medium", "Low"}
 var taskStatus = []string{"Not started", "In Progress", "Paused", "Stuck", "Done"}
+var taskStatusColors = map[string]color.Color{
+	"Not started": &color.Gray{},                            //Grey
+	"In Progress": &colornames.Orange,                       //Orange
+	"Paused":      &colornames.Blue,                         //Blue
+	"Stuck":       &color.NRGBA{R: 255, G: 0, B: 0, A: 255}, //Red
+	"Done":        &colornames.Green,                        //Green
+}
 var TODOColumns = []string{"ID", "Position", "Title", "Status", "Priority"}
 var TODOColumnsSize = []float32{1, 70, 600, 210, 70}
 
-func (td *TODO) getStatusField(id int64) *widget.Select {
-	s := widget.NewSelect(taskStatus, func(value string) {
-		log.Println("Select set to ", value)
-		log.Println(id)
+func (td *TODO) getStatusField(id int64) *CustomSelect {
+	s := NewCustomSelect(taskStatusColors, taskStatus, func(value string) {
 		td.DB.UpdateStatus(id, value)
 		if value == "Done" {
 			td.LoadTasks()
 			td.TaskTable.Refresh()
 		}
 	})
+
 	return s
 }
