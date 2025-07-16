d03d1969e327eff678e2a7336a9c41878b086009
Maurice Courtois <courtoisninja@gmail.com>
2025-07-16 23:20:43 +0200

    Add getPriorityField CustomSelect Field

--- task.go
+++ task.go
@@ -7,6 +7,13 @@ import (
 )
 
 var taskPriority = []string{"", "Critical", "Very High", "High", "Medium", "Low"}
+var taskPriorityColors = map[string]color.Color{
+	"Low":       &color.Gray{},                            //Grey
+	"Medium":    &colornames.Orange,                       //Orange
+	"High":      &colornames.Blue,                         //Blue
+	"Very High": &color.NRGBA{R: 255, G: 0, B: 0, A: 255}, //Red
+	"Critical":  &colornames.Green,                        //Green
+}
 var taskStatus = []string{"Not started", "In Progress", "Paused", "Stuck", "Done"}
 var taskStatusColors = map[string]color.Color{
 	"Not started": &color.Gray{},                            //Grey
@@ -27,5 +34,12 @@ func (td *TODO) getStatusField(id int64) *CustomSelect {
 		}
 	})
 
+	return s
+}
+
+func (td *TODO) getPriorityField(id int64) *CustomSelect {
+	s := NewCustomSelect(taskPriorityColors, taskPriority, func(value string) {
+		td.DB.UpdatePriority(id, value)
+	})
 	return s
 }
