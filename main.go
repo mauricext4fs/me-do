d7542419cc92d6ce4c386842150ff86f8bb4fa81
Maurice Courtois <courtoisninja@gmail.com>
2025-07-23 13:15:27 +0200

    WP: Adding Logger and making the DB Path customizable.

--- main.go
+++ main.go
@@ -8,17 +8,21 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 
 	_ "github.com/glebarez/go-sqlite"
 )
 
 type TODO struct {
-	App        fyne.App
-	DB         repository.Repository
-	MainWindow fyne.Window
-	UIElements UIElements
-	ID         int64
+	App         fyne.App
+	InfoLog     *log.Logger
+	ErrorLog    *log.Logger
+	CurrentFile fyne.URI
+	DB          repository.Repository
+	MainWindow  fyne.Window
+	UIElements  UIElements
+	ID          int64
 
 	Tasks     [][]interface{}
 	TaskTable *widget.Table
@@ -29,6 +33,10 @@ func main() {
 	a := app.NewWithID("ch.mauricext4fs.medo")
 	td.App = a
 	//a.Settings().SetTheme(&MyTheme{})
+
+	// Adding loggers
+	td.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	td.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	sqlDB, err := td.connectSQL()
 	if err != nil {
@@ -41,15 +49,32 @@ func main() {
 
 	td.MainWindow.SetContent(finalContent)
 	td.MainWindow.ShowAndRun()
+
 }
 
-func (p *TODO) connectSQL() (*sql.DB, error) {
+func (td *TODO) getDBPath() string {
+	dr, err := storage.Child(td.App.Storage().RootURI(), "Documents")
+	du := ""
+	if err != nil {
+		td.ErrorLog.Println("Could not determined the Documents directory. Failing back to the default fyne.io dir")
+		du = td.App.Storage().RootURI().Path()
+	}
+
+	du = dr.Path()
+
+	return du
+}
+
+func (td *TODO) connectSQL() (*sql.DB, error) {
 	path := ""
+
+	pathi := td.getDBPath()
+	td.InfoLog.Println("Path: ", pathi)
 
 	if os.Getenv("DB_PATH") != "" {
 		path = os.Getenv("DB_PATH")
 	} else {
-		path = p.App.Storage().RootURI().Path() + "/sql.db"
+		path = td.App.Storage().RootURI().Path() + "/sql.db"
 	}
 
 	db, err := sql.Open("sqlite", path)
@@ -60,10 +85,10 @@ func (p *TODO) connectSQL() (*sql.DB, error) {
 	return db, nil
 }
 
-func (p *TODO) setupDB(sqlDB *sql.DB) {
-	p.DB = repository.NewSQLiteRepository(sqlDB)
+func (td *TODO) setupDB(sqlDB *sql.DB) {
+	td.DB = repository.NewSQLiteRepository(sqlDB)
 
-	err := p.DB.Migrate()
+	err := td.DB.Migrate()
 	if err != nil {
 		log.Panic(err)
 	}
